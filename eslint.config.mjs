f7109114f5298cad8f9e49e1a3249202f348c519
Yoo TaeSeung <remicon99@gmail.com>
2025-01-22 03:50:32 +0900

    setting: tailwind eslint & prettier 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintPluginTailwindcss from "eslint-plugin-tailwindcss";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +12,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    name: "tailwindcss",
+    plugins: {
+      tailwindcss: eslintPluginTailwindcss,
+    },
+    rules: {
+      "tailwindcss/classnames-order": "error",
+    },
+  },
 ];
 
 export default eslintConfig;
