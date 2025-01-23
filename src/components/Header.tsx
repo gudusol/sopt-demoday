e9bb987176c2cb2b299fc58c6c4948cd39c58250
Yoo TaeSeung <remicon99@gmail.com>
2025-01-24 04:58:47 +0900

    fix: 헤더 위치 수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
   }, []);
 
   return (
-    <header className="fixed left-0 top-0 z-10 flex w-full max-w-[43rem] items-start justify-between bg-black p-[2rem] transition-all">
+    <header className="fixed top-0 z-10 flex w-full max-w-[43rem] items-start justify-between bg-black p-[2rem] transition-all">
       <Image
         src={"/images/logo.svg"}
         alt="logo-image"
