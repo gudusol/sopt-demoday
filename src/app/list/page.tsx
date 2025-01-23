008d40be464f0c6a951f337565d2ca5d52a5af1c
Yoo TaeSeung <remicon99@gmail.com>
2025-01-24 07:03:47 +0900

    feat: list 페이지 퍼블리싱

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,5 +1,63 @@
-const page = () => {
-  return <div>list page</div>;
+"use client";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import ProductList from "@/components/ProductList";
+import { listData } from "../../../public/data/listData";
+import { useState } from "react";
+
+const List = () => {
+  const [menu, setMenu] = useState<"appjam" | "sopt-term" | "makers">("appjam");
+
+  return (
+    <>
+      <Header selectedMenu="product" isSmallLogo={true} />
+      <main className="w-full px-[2rem] pt-[7.6rem]">
+        <nav className="w-full">
+          <ul className="flex gap-[0.9rem]">
+            <li
+              className={`font-title4_sb_15 flex h-[3.3rem] flex-[2] items-center justify-center border ${
+                menu === "appjam"
+                  ? "bg-w100 font-bold text-black"
+                  : "bg-black text-w100"
+              }`}
+              onClick={() => setMenu("appjam")}
+            >
+              앱잼
+            </li>
+            <li
+              className={`font-title4_sb_15 flex h-[3.3rem] flex-[1] items-center justify-center border ${
+                menu === "sopt-term"
+                  ? "bg-w100 font-bold text-black"
+                  : "bg-black text-w100"
+              }`}
+              onClick={() => setMenu("sopt-term")}
+            >
+              솝텀
+            </li>
+            <li
+              className={`font-title4_sb_15 flex h-[3.3rem] flex-[1] items-center justify-center border ${
+                menu === "makers"
+                  ? "bg-w100 font-bold text-black"
+                  : "bg-black text-w100"
+              }`}
+              onClick={() => setMenu("makers")}
+            >
+              메이커스
+            </li>
+          </ul>
+        </nav>
+
+        <section className="mb-[6rem] mt-[2.3rem]">
+          {menu === "appjam" && <ProductList products={listData.appjam} />}
+          {menu === "sopt-term" && (
+            <ProductList products={listData["sopt-term"]} />
+          )}
+          {menu === "makers" && <ProductList products={listData.makers} />}
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
-export default page;
+export default List;
---
+++ src/components/ProductList.tsx
@@ -0,0 +1,26 @@
+import { listDataType } from "../../public/data/listData";
+import ListCard from "./ListCard";
+
+interface ProductListProps {
+  products: listDataType[];
+}
+
+const ProductList = ({ products }: ProductListProps) => {
+  return (
+    <div className="grid grid-cols-2 gap-x-[1.3rem] gap-y-[2.6rem]">
+      {products.map((product) => {
+        return (
+          <ListCard
+            key={product.name}
+            imageUrl={product.imageUrl}
+            name={product.name}
+            category={product.category}
+            description={product.description}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default ProductList;
