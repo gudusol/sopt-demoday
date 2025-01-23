5f5c9efef73f2186e326016ff929e7377012f6ad
Yoo TaeSeung <remicon99@gmail.com>
2025-01-24 01:17:46 +0900

    feat: Header 컴포넌트

---
+++ src/components/Header.tsx
@@ -0,0 +1,35 @@
+"use client";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+
+export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <header className="fixed left-0 top-0 z-10 flex w-full max-w-[43rem] items-start justify-between bg-black p-[2rem] transition-all duration-300">
+      <Image
+        src={"/images/logo.svg"}
+        alt="logo-image"
+        width={isScrolled ? 105 : 225}
+        height={isScrolled ? 41 : 88}
+        className="object-contain transition-all duration-300"
+      />
+
+      <Image
+        src="/icons/hamburger.svg"
+        alt="hamburger-menu"
+        width={30}
+        height={23}
+      />
+    </header>
+  );
+}
