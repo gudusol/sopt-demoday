33b659311b153193f077b78e350a723ba873e001
Yoo TaeSeung <remicon99@gmail.com>
2025-01-24 19:30:39 +0900

    feat: 앱잼 프로덕트 랜덤으로 섞어서 출력

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -2,11 +2,19 @@
 import Header from "@/components/Header";
 import ProductList from "@/components/ProductList";
 import { listData } from "../../../public/data/listData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const shuffleArray = <T,>(array: T[]): T[] => {
+  return [...array].sort(() => Math.random() - 0.5);
+};
 
 const List = () => {
   const [menu, setMenu] = useState<"appjam" | "sopt-term" | "makers">("appjam");
+  const [shuffledAppjamList, setShuffledAppjamList] = useState(listData.appjam);
 
+  useEffect(() => {
+    setShuffledAppjamList(shuffleArray(listData.appjam));
+  }, []);
   return (
     <>
       <Header selectedMenu="product" isSmallLogo={true} />
@@ -14,7 +22,7 @@ const List = () => {
         <nav className="w-full">
           <ul className="flex gap-[0.9rem]">
             <li
-              className={`font-title4_sb_15 flex h-[3.3rem] flex-[2] items-center justify-center border ${
+              className={`font-title4_sb_15 flex h-[3.3rem] flex-[2] cursor-pointer items-center justify-center border ${
                 menu === "appjam"
                   ? "bg-w100 font-bold text-black"
                   : "bg-black text-w100"
@@ -24,7 +32,7 @@ const List = () => {
               앱잼
             </li>
             <li
-              className={`font-title4_sb_15 flex h-[3.3rem] flex-[1] items-center justify-center border ${
+              className={`font-title4_sb_15 flex h-[3.3rem] flex-[1] cursor-pointer items-center justify-center border ${
                 menu === "sopt-term"
                   ? "bg-w100 font-bold text-black"
                   : "bg-black text-w100"
@@ -34,7 +42,7 @@ const List = () => {
               솝텀
             </li>
             <li
-              className={`font-title4_sb_15 flex h-[3.3rem] flex-[1] items-center justify-center border ${
+              className={`font-title4_sb_15 flex h-[3.3rem] flex-[1] cursor-pointer items-center justify-center border ${
                 menu === "makers"
                   ? "bg-w100 font-bold text-black"
                   : "bg-black text-w100"
@@ -47,7 +55,7 @@ const List = () => {
         </nav>
 
         <section className="mb-[6rem] mt-[2.3rem]">
-          {menu === "appjam" && <ProductList products={listData.appjam} />}
+          {menu === "appjam" && <ProductList products={shuffledAppjamList} />}
           {menu === "sopt-term" && (
             <ProductList products={listData["sopt-term"]} />
           )}
