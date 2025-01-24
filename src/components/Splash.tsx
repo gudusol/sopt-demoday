48766eadc9dcc1cf555465c4cf7547f54886a800
Yoo TaeSeung <remicon99@gmail.com>
2025-01-25 07:32:39 +0900

    feat: 스플래시 화면 스크롤 막기

--- src/components/Splash.tsx
+++ src/components/Splash.tsx
@@ -1,6 +1,17 @@
+"use client";
 import Image from "next/image";
+import { useEffect } from "react";
 
 const Splash = () => {
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    document.body.style.pointerEvents = "none";
+
+    return () => {
+      document.body.style.overflow = "auto";
+      document.body.style.pointerEvents = "auto";
+    };
+  }, []);
   return (
     <div
       className="fixed left-0 top-0 z-50 flex h-[100dvh] w-full items-center justify-center bg-cover bg-center bg-no-repeat"
